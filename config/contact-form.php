5c389d3f76b4d0183e6814194c7e6da79a4f8f94
uenoryo <uenoryo@uma-b.com>
2019-08-23 23:58:31 +0900

    問い合わせできるように.

---
+++ config/contact-form.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'toEmail'             => 'admin@admin.com',
+    'prependSubject'      => '送ったよ',
+    'prependSender'       => '送られたよ',
+    'allowAttachments'    => false,
+    'successFlashMessage' => 'Message sent!'
+];
